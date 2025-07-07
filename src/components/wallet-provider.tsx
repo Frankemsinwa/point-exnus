7c4edf79ca25ff2ae5a6ea6134561af1484467c5
Frank Emesinwa <frankemesinwa@gmail.com>
2025-07-07 06:02:53 +0000

    remove the wallet adapter from testnet to mainet

--- src/components/wallet-provider.tsx
+++ src/components/wallet-provider.tsx
@@ -14,7 +14,7 @@ export const WalletProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
